4f5bcea3052a27334ed704568099228a38cf3060
liumstone <am.kushal02@gmail.com>
2023-12-06 22:47:27 +0530

    Add files via upload

--- learner-site/ed_line/urls.py
+++ learner-site/ed_line/urls.py
@@ -10,17 +10,22 @@ urlpatterns = [
     # HOME ROUTING
     path('', r.goto_homepage_or_landing, name='redirections'),
     path('home/', r.homepage, name='dash-homepage'),              # <---- for the signed-in
+    path('home/courses/', r.courses, name='home-courses'),              # <---- for the signed-in             # <---- for the signed-in
+
+
 
     # SIGNIN AND LOGIN
     path('signup/', r.signup, name='user-signup'),
     path('login/', r.login, name='user-login'),
     path('logout/', r.logout, name='user-logout'),
 
-    # USER PROFILE
-    path('user/', r.show_user_profile, name='show-user'),
-    
-    # DASHBOARD: MAIN PAGE
 
+
+    # USER PROFILE
+    path('user/', r.goto_user_profile, name='redirect-to-profile'),
+    path('user/profile/', r.show_user_profile, name='show-user'),
+    path('user/enrollments/', r.user_enrollments, name='user-enrollments'),              # <---- for the signed-in
+    path('user/settings/', r.user_settings, name='user-settings'),              # <---- for the signed-in
 
 
     # REST UNDEFINED PATTERNS
