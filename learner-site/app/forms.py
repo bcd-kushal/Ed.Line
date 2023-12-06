cbe31105fc26f25ea952b53b2af4e822a1391bc9
liumstone <am.kushal02@gmail.com>
2023-12-06 22:50:20 +0530

    Add files via upload

---
+++ learner-site/app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+                            max_length=100
+                        )
+    password = forms.CharField(
+                            widget=forms.PasswordInput()
+                        )
+
+
+
+
+class SignupForm(forms.Form): 
+    user = forms.CharField(max_length=100)   # mobile number: PK
+    fname = forms.CharField(max_length=100)
+    lname = forms.CharField(max_length=100)
+    email = forms.CharField(
+                            max_length=100, 
+                            widget=forms.EmailInput()
+                        )
+    pass1 = forms.CharField(
+                            widget=forms.PasswordInput()
+                        )
+
+
