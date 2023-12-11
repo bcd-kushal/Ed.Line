5dc483c283e7d2080a28672c178eac2322d836d7
liumstone <am.kushal02@gmail.com>
2023-12-11 23:34:51 +0530

    Add files via upload

---
+++ learner-site/app/courses.py
@@ -0,0 +1,186 @@
+def all_courses():
+    return {
+
+        
+                "development": [
+                    "Web Dev",
+                    "Data Science",
+                    "Mobile Dev", 
+                    "Programming", 
+                    "Game Dev", 
+                    "Databases", 
+                    "Software Testing", 
+                    "Software Engineering", 
+                    "Deve Tools", 
+                    "No Code Dev"
+                    ],
+
+
+                "business": [
+                    "Enterpreneurship",
+                    "Communication",
+                    "Management",
+                    "Sales",
+                    "Business Strategy",
+                    "Operations",
+                    "Project Management",
+                    "Business Law",
+                    "Business Analytics & Intelligence",
+                    "Human Resources",
+                    "Industry",
+                    "E-Commerce",
+                    "Media",
+                    "Real Esate"
+                    ],
+
+
+                "finance-and-accounting": [
+                    "Accounting & Bookkeeping",
+                    "Compliance",
+                    "Cryptocurrency & Blockchain",
+                    "Economics",
+                    "Finance",
+                    "Finace Cert & Exam Prep",
+                    "Financial Modelling & Analysis",
+                    "Investing & Trading",
+                    "Money Management Tools",
+                    "Taxes",
+                    "Other Finance & Accounting"
+                    ],
+
+
+                "it-and-software": [
+                    "IT Certifications",
+                    "Network Security",
+                    "Hardware",
+                    "Operating System & Servers",
+                    "Other IT & Software"
+                    ],
+
+
+                "office-productivity": [
+                    "Microsoft",
+                    "Apple",
+                    "Google",
+                    "SAP",
+                    "Oracle",
+                    "Other Office Productivity"
+                    ],
+
+
+                "personal-development": [
+                    "Personal Transformation",
+                    "Personal Productivity",
+                    "Leadership",
+                    "Career Development",
+                    "Parenting & Relationship",
+                    "Happiness",
+                    "Esoteric Practice",
+                    "Religion & Spritiuality",
+                    "Personal Brand Building",
+                    "Creativity",
+                    "Infuence",
+                    "Self Esteem & Confidence",
+                    "Stress Management",
+                    "Memory & Study Skills",
+                    "Motivation",
+                    "Other Personal Developement"
+                    ],
+
+
+                "design": [
+                    "Web Design",
+                    "Graphic Design & Illustration",
+                    "Design Tools",
+                    "User Experience Design",
+                    "Game Design",
+                    "3D & Animation",
+                    "Fashion Design",
+                    "Architectural Design",
+                    "Ineterior Design",
+                    "Other Design"
+                    ],
+
+
+                "marketing": [
+                    "Digital Marketing",
+                    "Search Engine Optimization",
+                    "Social Media Marketing",
+                    "Branding",
+                    "Marketing Fundaments",
+                    "Marketing Analytics & Automation",
+                    "Public Relations",
+                    "Paid Advertising",
+                    "Video & Mobile Marketing",
+                    "Content Markting",
+                    "Growth Hacking",
+                    "Affiliate Marketing",
+                    "Product Marketing",
+                    "Other Marketing",
+                    "Other Lifestyle"
+                    ],
+
+
+                "lifestyle": [
+                    "Arts & Crafts",
+                    "Beauty & Makeup",
+                    "Esoteric Practice",
+                    "Food & Beverage",
+                    "Home Improvement & Gardening",
+                    "Pet Care & Training",
+                    "Travel"
+                    ],
+
+
+                "photography-and-video": [
+                    "Digital Photography",
+                    "Photography",
+                    "Portrait Photography",
+                    "Photography Tools",
+                    "Commercial Photography",
+                    "Video Design",
+                    "Other Photography & Video"
+                    ],
+
+
+                "health-and-fitness": [
+                    "Fitness",
+                    "General Health",
+                    "Sports",
+                    "Nutrition & Diet",
+                    "Yoga",
+                    "Mental Health",
+                    "Martial Arts & Defense",
+                    "Safety & First Aid",
+                    "Dance",
+                    "Meditation",
+                    "Other Health Fitness"
+                    ],
+
+
+                "music": [
+                    "Instruments",
+                    "Music Production",
+                    "Music Fundamentals",
+                    "Vocal",
+                    "Music Techniques",
+                    "Music Software",
+                    "Other Music"
+                    ],
+
+
+                "teaching-and-academics": [
+                    "Engineering",
+                    "Humanities",
+                    "Math",
+                    "Science",
+                    "Online Education",
+                    "Social Science",
+                    "Language Training",
+                    "Teacher Training",
+                    "Test Prep",
+                    "Other Teaching & Academics"
+                    ]
+
+
+}
