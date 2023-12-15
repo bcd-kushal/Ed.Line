14641a35b6dd861999a85f8e929bfb858c5fddd6
winter <am.kushal02@gmail.com>
2023-12-15 21:28:31 +0530

    Add files via upload

---
+++ learner-site/app/footer.py
@@ -0,0 +1,97 @@
+def footer_data():
+    return {
+        "companyGrid": {
+            "title": "Company",
+            "data": [
+                { "About": "/about-us/" },
+                { "Careers": "/careers/" },
+                { "Blogs": "/blogs/" },
+                { "Privacy Policy": "/privacy/" },
+                { "Terms & Conditions": "/terms/" },
+            ]
+        },
+
+        "helpSupportGrid": {
+            "title": "Help & Support",
+            "data": [
+                { "User Guidelines": "/about-us/" },
+                { "Site Map": "/careers/" },
+                { "Refund Policy": "/blogs/" },
+                { "Takedown Policy": "/privacy/" },
+                { "Grievance Redressal": "/terms/" },
+            ]
+        },
+        
+        "checkOut": {
+            "title": "Products",
+            "data": [
+                { '<i class="fa-solid fa-square-up-right fa-lg" style="color: rgb(83, 140, 215);"></i> &nbsp;Educator App': "/terms/" },
+            ]
+        },
+        
+        "popularGoals": {
+            "title": "Popular courses",
+            "data": [
+                { "Office Productivity": "/about-us/" },
+                { "Web Development": "/careers/" },
+                { "Marketing": "/blogs/" },
+                { "Music": "/privacy/" },
+                { "Health & Fitness": "/terms/" },
+            ]
+        },
+        
+        "EdLineCentre": {
+            "title": "Ed.Line Centre",
+            "data": [
+                { "BHU Web Dev": "/about-us/" },
+                { "Delhi Foundation": "/careers/" },
+                { "Gurgaon HR": "/blogs/" },            ]
+        },
+        
+        "studyMaterial": {
+            "title": "Study Material",
+            "data": [
+                { "Web Study Material": "/about-us/" },
+                { "ML/AI Study Material": "/careers/" },
+                { "CSX Study Material": "/blogs/" },
+                { "SEO Study Material": "/privacy/" },
+                { "HR Study Materia": "/terms/" },
+            ]
+        },
+        
+    }
+
+
+
+
+
+
+
+
+def footer_socials():
+    return {
+        "insta": {
+            "icon": "fa-brands fa-instagram fa-sm",
+            "link": "https://www.instagram.com/edline_devs/"
+        },
+        
+        "youtube": {
+            "icon": "fa-brands fa-youtube fa-sm",
+            "link": "https://www.youtube.com/channel/UCl0oLctFKRfSLf6OLrjOKRw/"
+        },
+        
+        "facebook": {
+            "icon": "fa-brands fa-meta fa-sm",
+            "link": "https://www.facebook.com/profile.php?id=61554715046017/"
+        },
+        
+        "twitter": {
+            "icon": "fa-brands fa-x-twitter fa-sm",
+            "link": "https://twitter.com/edline_devs/"
+        },
+        
+        "linkedin": {
+            "icon": "fa-brands fa-linkedin fa-sm",
+            "link": "https://www.linkedin.com/in/edline-devs/"
+        },
+    }
