df9fe3a35b12ae6ba01993c0a4b7a7d4504ee11d
winter <am.kushal02@gmail.com>
2023-12-16 17:55:30 +0530

    Add files via upload

---
+++ educator-site/ed_teach/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from app import views as r
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # HOME ROUTING
+    path('', r.goto_homepage_or_landing, name='redirections'),
+    path('home/', r.homepage, name='dash-homepage'),
+
+    # SIGNIN AND LOGIN
+    path('signup/', r.signup, name='user-signup'),
+    path('login/', r.login, name='user-login'),
+    path('logout/', r.logout, name='user-logout'),
+
+    #ADD COURSE
+    path('add-course/', r.new_course, name='add-course'),
+
+
+
+    
+    # REST UNDEFINED PATTERNS
+    re_path(r'^.*/$', r.bad_route, name='bad-route'),
+
+]
+
+
+urlpatterns += static(settings.MEDIA_URI,document_root=settings.MEDIA_ROOT)
