41b527064348a2b7bbc7ffb3bf88e431a372ed96
winter <am.kushal02@gmail.com>
2023-12-20 16:00:10 +0530

    Add files via upload

--- educator-site/ed_teach/urls.py
+++ educator-site/ed_teach/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 from app import views as r
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
@@ -20,9 +19,37 @@ urlpatterns = [
     path('login/', r.login, name='user-login'),
     path('logout/', r.logout, name='user-logout'),
 
-    #ADD COURSE
-    path('add-pfp/', r.user_pfp, name='add-pfp'),
-    path('add-course/', r.add_course, name='add-course'),
+    # COURSE
+    path('course/analytics/', r.course_analytics, name='analytics'),
+
+
+    # ADD COURSE
+    path('add-course/', r.redirect_to_course_overview, name='course-redirect'),
+    path('add-course-overview/', r.add_course_overview, name='add-course'),
+    path('add-course-data/', r.add_course_data, name='add-course-data'),
+
+
+    # EDUCATOR
+    path('educator/courses/', r.educator_courses, name='educator-courses'),
+    path('educator/profile/', r.educator_profile, name='educator-profile'),
+    path('educator/settings/', r.educator_settings, name='educator-settings'),
+
+    
+    path('educator/update-pfp/', r.user_pfp, name='profile-pic-update'),
+    path('educator/<str:to_edit>/', r.user_edit, name='user-details-edit'),
+    
+    # path('educator/update-mobile/', r.user_pfp, name='mobile-update'),
+    # path('educator/update-email/', r.user_pfp, name='email-update'),
+    
+    path('privacy/', r.privacy, name='privacy'),     
+    path('terms/', r.terms, name='terms'),     
+    
+    path('about-us/', r.about_us, name='about-us'),      
+    path('careers/', r.careers, name='careers'),     
+    path('history/', r.history, name='history'),      
+    path('gallery/', r.gallery, name='gallery'),          
+    path('blogs/', r.blogs, name='blogs'),             
+    path('contact-us/', r.contact_us, name='contact-us'),  
 
 
 
