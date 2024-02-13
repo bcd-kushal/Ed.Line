87167d87d8b462eab1c067e59043103c50695137
Kushal <dev@kushalkumarsaha.com>
2024-02-13 23:52:16 +0530

    Add files via upload

--- educator-site/app/admin.py
+++ educator-site/app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import educator_pictures
+from .models import *
 
 admin.site.register(educator_pictures)
+admin.site.register(CourseOverview)
+admin.site.register(HeaderTitles)
+admin.site.register(VideoTitles)
+admin.site.register(Videos)
 
 # Register your models here.
